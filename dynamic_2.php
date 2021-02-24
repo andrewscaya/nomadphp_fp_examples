d9137301fa2ef75f33d1b219fe81698bcf8d2492
Andrew Caya <andrewscaya@yahoo.ca>
2021-02-24 16:58:26 -0500

    2021-02-25 AC: Adds the code examples.

---
+++ dynamic_2.php
@@ -0,0 +1,27 @@
+<?php
+// Statically named function with tail-calling, and dynamic approach
+
+// chap3_dynamic_2.php
+
+$start = microtime(true);
+
+$x = 1;
+
+$data = [];
+
+function populateArray(Array $data, $x)
+{
+    $data[$x] = $x;
+
+    $x++;
+
+    return $x <= 1000 ? populateArray($data, $x) : $data;
+}
+
+$data = populateArray($data, $x);
+
+$time = microtime(true) - $start;
+
+echo 'Time elapsed: ' . $time . PHP_EOL;
+
+echo memory_get_usage() . ' bytes' . PHP_EOL;
---
+++ dynamic_4.php
@@ -0,0 +1,27 @@
+<?php
+// Structural with hint of functional programming (generator) and dynamic approach
+
+// chap3_dynamic_4.php
+
+$start = microtime(true);
+
+$data = [];
+
+function populateArray()
+{
+    for ($i = 1; $i <= 1000; $i++) {
+        yield $i => $i;
+    }
+
+    return;
+}
+
+foreach (populateArray() as $key => $value) {
+    $data[$key] = $value;
+}
+
+$time = microtime(true) - $start;
+
+echo 'Time elapsed: ' . $time . PHP_EOL;
+
+echo memory_get_usage() . ' bytes' . PHP_EOL;
